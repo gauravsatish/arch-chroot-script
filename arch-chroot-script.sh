66b2a16f33dcb3c5ccec016757f313e6d9c1077a
Gaurav Satish <59788284+gauravsatish@users.noreply.github.com>
2022-09-18 08:45:06 +0530

    Add files via upload

---
+++ arch-chroot-script.sh
@@ -0,0 +1,8 @@
+sudo mkdir -p /mnt/boot/efi
+sudo mount /dev/sda1 /mnt/boot/efi
+sudo mount -o subvol=@ /dev/sda6 /mnt
+sudo mount -o subvol=@cache /dev/sda6 /mnt/var/cache
+sudo mount -o subvol=@log /dev/sda6 /mnt/var/log
+sudo mount -o subvol=@home /dev/sda6 /mnt/home
+
+sudo arch-chroot /mnt
